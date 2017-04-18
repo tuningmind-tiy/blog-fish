48f621c34b3421d1e3ad44e85d3837e62566c593
tuningmind <tuningmind@gmail.com>
2017-04-18 17:42:42 -0600

    Add index.js for express

---
+++ index.js
@@ -0,0 +1,14 @@
+let express = require('express')
+let app = express()
+
+app.set('port', (process.env.PORT || 3000))
+
+app.use(express.static(__dirname + '/build'))
+
+app.get('*', function(request, response) {
+  response.sendFile(__dirname + '/build/index.html')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Express server started on port", app.get('port'))
+})
